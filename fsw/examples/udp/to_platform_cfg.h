ae00be772d9ccd7cbdc451bf1b38e3d750556f14
Jennifer O'Palko <jennifero19@hotmail.com>
2026-07-28 18:45:39 +0000

    Add TO_MAX_TBL_ENTRIES define to udp transport variant for CFDP

--- fsw/examples/udp/to_platform_cfg.h
+++ fsw/examples/udp/to_platform_cfg.h
@@ -37,7 +37,7 @@ extern "C" {
 #define TO_NUM_CRITICAL_MIDS     3
 #define TO_DEFAULT_DEST_PORT     5011 
 #define TO_WAKEUP_TIMEOUT        50
-
+#define TO_MAX_TBL_ENTRIES        110
 #define TO_SCH_PIPE_DEPTH  32
 #define TO_CMD_PIPE_DEPTH  32
 #define TO_TLM_PIPE_DEPTH  32
